74a99f4b12a91a1e14aa4b9ad07c5989ab27450d
AbdrasulovAdilet <abdrasulovadilet1@gmail.com>
2024-11-22 15:38:33 +0600

    Update main.py
    .

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import random
 total_weight = 713
+weights = sorted([random.randint(1, total_weight - 2) for _ in range(2)])
+a = weights[0]
+b = weights[1] - weights[0]
+c = total_weight - weights[1]
+weight = [a, b, c]

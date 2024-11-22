d57e8541f885388b11d1fc107996989cab114df1
AbdrasulovAdilet <abdrasulovadilet1@gmail.com>
2024-11-22 15:46:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
+from random import sample
 total_weight = 713
 weights = sorted([random.randint(1, total_weight - 2) for _ in range(2)])
 a = weights[0]
 b = weights[1] - weights[0]
 c = total_weight - weights[1]
 weight = [a, b, c]
+list = [1, 2, 3, 4, 5, 6, 7]
+positions = sample(list, 3)
+print("There are 3 boxes between 1 and 7 kilometers. You have to find all 3 boxes, and their total weight must be 713kg.")

dba3cf77fe5fa870babdc5f4cfb3a4d235b123aa
AbdrasulovAdilet <abdrasulovadilet1@gmail.com>
2024-11-24 21:59:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ weight = [a, b, c]
 list = [1, 2, 3, 4, 5, 6, 7]
 positions = sample(list, 3)
 print("There are 3 boxes between 1 and 7 kilometers. You have to find all 3 boxes, and their total weight must be 713kg.")
+while True:
+    guesses = []
+
+    for _ in range(3):
+        guess = int(input("Enter distance:"))
+        guesses.append(guess)
+    for guess in guesses:
+        if guess in positions:
+            weight = weights[positions.index(guess)]

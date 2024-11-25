57ccac9c1f056b87f0cf18af37a1369be8afa4e3
AbdrasulovAdilet <abdrasulovadilet1@gmail.com>
2024-11-25 16:44:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ a = weights[0]
 b = weights[1] - weights[0]
 c = total_weight - weights[1]
 weight = [a, b, c]
-list = [1, 2, 3, 4, 5, 6, 7]
-positions = sample(list, 3)
+positions = sample(range(1, 8), 3)
 print("There are 3 boxes between 1 and 7 kilometers. You have to find all 3 boxes, and their total weight must be 713kg.")
-found_weight = []
+found_weights = []
 while True:
+    found_weights.clear()
     guesses = []
 
     for _ in range(3):
         guess = int(input("Enter distance:"))
         guesses.append(guess)
+    
     for guess in guesses:
         if guess in positions:
-            weight = weights[positions.index(guess)]
-            found_weight.append(weight)
+            found_weights.append(weights[positions.index(guess)])
+
+    if sum(found_weights) == total_weight and len(found_weights) == 3:
+        print("\nCongratulations! You found all the cargo!")
+        print("Box weights:", found_weights)
+        break
+    else:
+        print("\nWrong guesses! The boxes are moving to new locations...")
+        positions = random.sample(range(1, 8), 3)

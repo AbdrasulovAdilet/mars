cf11d7651b1517b423461bdabeb0208ffe44cae6
AbdrasulovAdilet <abdrasulovadilet1@gmail.com>
2024-11-25 18:16:11 +0600

    helping friends.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import random
 from random import sample
+
 total_weight = 713
-weights = sorted([random.randint(1, total_weight - 2) for _ in range(2)])
-weights = [weights[0], weights[1] - weights[0], total_weight - weights[1]]
+box1 = random.randint(1, total_weight)
+box2 = random.randint(1, total_weight - box1)
+box3 = total_weight - box1 - box2
+found_weights = [box1, box2, box3]
+
 positions = sample(range(1, 8), 3)
 print("There are 3 boxes between 1 and 7 kilometers. You have to find all 3 boxes, and their total weight must be 713kg.")
-found_weights = []
+
 while True:
-    found_weights.clear()
     guesses = []
     
     for _ in range(3):
         guess = int(input("Enter kilometer mark: "))
         guesses.append(guess)
-    
-    for guess in guesses:
-        if guess in positions:
-            found_weights.append(weights[positions.index(guess)])
-    
-    if sum(found_weights) == total_weight and len(found_weights) == 3:
+
+    if sorted(guesses) == sorted(positions):
         print("\nCongratulations! You found all the cargo!")
         print("Box weights:", found_weights)
         break
